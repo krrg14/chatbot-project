1efe76cb915428f3b15cb61ad11834ed8ebd1544
krrg14 <krrakshithg@gmail.com>
2026-04-13 18:26:49 +0000

    updated file

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.sidebar.title('⚙️ Settings')
 api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
 temperature = st.sidebar.slider("Temperature", 0.0, 1.0, 0.7)
 tokens = st.sidebar.slider("Max Tokens", 50, 500, 100)
-engine = st.sidebar.selectbox("Select engine", ["gpt-4.1-2025-04-14", "gpt-5-mini", "gpt-4o-mini", "gpt-4-turbo","gpt-4"])
+engine = st.sidebar.selectbox("Select engine", ["gpt-4.1-2025-04-14", "gpt-5-mini", "gpt-5", "gpt-4o-mini", "gpt-4-turbo","gpt-4"])
 
 # --- Function to Generate Response ---
 def generate_response(question, api_key, temperature, tokens, engine):
